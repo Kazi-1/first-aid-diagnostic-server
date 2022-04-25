194729e39a7773871b82d117eb98f1f61cc7cb79
kazi-coder <kazi-coder@github.com>
2022-04-25 21:56:24 +0200

    .env file fixed

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const { MongoClient } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
-const serviceAccount = require('./first-aid-diagnostic-firebase-adminsdk.json');
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = require('./first-aid-diagnostic-firebase-adminsdk.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
