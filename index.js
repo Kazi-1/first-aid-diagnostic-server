74d6e813360a2376948aa4c9aeee80608452f404
kazi-coder <kazi-coder@github.com>
2022-05-08 23:50:21 +0200

    admin service fixed

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const { MongoClient } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
-// const serviceAccount = require('./first-aid-diagnostic-firebase-adminsdk.json');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+const serviceAccount = require('./first-aid-diagnostic-firebase-adminsdk.json');
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 
 app.use(cors());
 app.use(express.json());
@@ -89,8 +91,18 @@ async function run() {
       res.json(result);
     });
 
+    // trying......
+    // app.put('/users/admin', async (req, res) => {
+    //   const user = req.body;
+    //   console.log("put", user);
+    //   const filter = { email: user.email };
+    //   const updateDoc = { $set: { role: 'admin' } };
+    //   const result = await usersCollection.updateOne(filter, updateDoc);
+    //   res.json(result);
+    // })
+
+
     app.put('/users/admin', verifyToken, async (req, res) => {
-      
       const user = req.body;
       console.log("put", req.decodedEmail);
       const requester = req.decodedEmail;
@@ -104,7 +116,7 @@ async function run() {
         }
       }
       else {
-        res.status(403).json({message: 'you do not have access to make admin'})
+        res.status(403).json({ message: 'you do not have access to make admin' })
       }
 
     })
